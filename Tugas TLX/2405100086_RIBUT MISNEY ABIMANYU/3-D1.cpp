17cfb045462f3a6c986b6a39eb00eca39eed0bce
abimanyu-rma <ributabimanyu.abi@gmail.com>
2024-10-11 09:43:23 +0700

    Create 3-D1.cpp

---
+++ Tugas TLX/2405100086_RIBUT MISNEY ABIMANYU/3-D1.cpp
@@ -0,0 +1,10 @@
+/*Berapa kalikah C++ akan tercetak?*/
+int counter = 0;
+while (counter < 4) {
+    cout << "C++" << endl;
+    counter += 1;
+}
+//c++ tercetak 4 karena counter akan bertambah satu setiap iterasi loop,
+sementara kondisi while (counter < 4) masih dipenuhi.
+Karena nilai awal counter adalah 0, maka loop akan berlangsung sebanyak empat kali.//
+
